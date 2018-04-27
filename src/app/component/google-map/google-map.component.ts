36a6d255675f3ca912a6681a2e02d0e74f5c0bf6
Kai Chen <kchen9530@gmail.com>
2018-04-26 23:07:01 -0400

    add center

--- src/app/component/google-map/google-map.component.ts
+++ src/app/component/google-map/google-map.component.ts
@@ -12,9 +12,30 @@ export class GoogleMapComponent implements OnInit {
     myMap;
     hotelMarkers = [];
     hotels: Hotel[];
-    tagItems: {[tag: string]: Hotel[]} = {};
+    hotelList: Hotel[];
+    tagItems: { [tag: string]: Hotel[] } = {};
+    tags: string[] = [];
+    centerMarker: google.maps.Marker;
 
     constructor(private userServie: UserService) { }
+
+    addCenter(map, latLng) {
+        if (map.centerMarker !== undefined) {
+            map.centerMarker.setMap(null);
+        }
+        var icon = {
+            url: "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png",
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
+        map.centerMarker = new google.maps.Marker({
+            map: map,
+            position: latLng,
+            icon: icon
+        });
+    }
 
     ngOnInit() {
         this.initAutocomplete();
@@ -25,6 +46,34 @@ export class GoogleMapComponent implements OnInit {
     //     [40.7000000, -74.0000000]
     //     // [-87.62374260000001,41.8952022],
     // ];
+    clickTag(tag) {
+        console.log(tag);
+        this.filter(true, tag);
+    }
+
+    filter(isFilter, targetTag?) {
+        console.log(targetTag);
+        this.hotelMarkers = [];
+        this.hotelList = [];
+
+        this.hotels.forEach(hotel => {
+            // if (!isFilter || hotel.tags.findIndex(targetTag) > -1) {
+            //     this.hotelList.push(hotel);
+            // }
+            // if ()
+            this.hotelList.push(hotel);
+        });
+
+        this.hotelList.forEach(hotel => {
+            this.hotelMarkers.push(new google.maps.Marker({
+                map: this.myMap,
+                title: hotel.name,
+                position: new google.maps.LatLng(hotel.lat, hotel.lng)
+            }));
+            console.log(hotel);
+        });
+
+    }
 
     recommend() {
         // this.addMarkers();
@@ -36,37 +85,44 @@ export class GoogleMapComponent implements OnInit {
         });
 
         this.tagItems = {};
+        this.tags = [];
 
         // add tags
         this.hotels.forEach(hotel => {
             hotel.tags.forEach(tag => {
-                this.tagItems[tag].push(hotel);
+                console.log(tag);
+                if (this.tagItems[tag] === undefined) {
+                    this.tagItems[tag] = [hotel];
+
+                } else {
+                    var tmp = this.tagItems[tag];
+                    this.tagItems[tag].push(hotel);
+                }
+
             });
         });
 
-        this.hotels.forEach(hotel => {
-            this.hotelMarkers.push(new google.maps.Marker({
-                map: this.myMap,
-                title: hotel.name,
-                position: new google.maps.LatLng(hotel.lat, hotel.lng)
-            }));
-            console.log(hotel);
-        });
+        for (var tag in this.tagItems) {
+            this.tags.push(tag);
+        }
 
-        
+        this.filter(false);
+
     }
 
     clear() {
         this.hotelMarkers.forEach(function (marker) {
             marker.setMap(null);
         });
-        this.hotels = [];
+        this.hotelList = [];
+        this.tagItems = {};
+        this.tags = [];
     }
-    
+
 
     // addMarkers() {
-        
-        
+
+
     // }
 
 
@@ -80,6 +136,29 @@ export class GoogleMapComponent implements OnInit {
             // mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
+        let that = this;
+        google.maps.event.addListener(map, 'click', function (event) {
+            console.log(event.latLng);
+            
+            that.addCenter(map, event.latLng);
+            // that.addCenterMarker(event.latLng);
+            // if (that.centerMarker !== undefined) {
+            //     that.centerMarker.setMap(null);
+            // }
+            // var icon = {
+            //     url: "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png",
+            //     size: new google.maps.Size(71, 71),
+            //     origin: new google.maps.Point(0, 0),
+            //     anchor: new google.maps.Point(17, 34),
+            //     scaledSize: new google.maps.Size(25, 25)
+            // };
+            // that.centerMarker = new google.maps.Marker({
+            //     map: map,
+            //     position: event.latLng,
+            //     icon: icon
+            // });
+        });
+
         this.myMap = map;
 
         // Create the search box and link it to the UI element.
@@ -88,9 +167,9 @@ export class GoogleMapComponent implements OnInit {
         // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
         // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function () {
-            searchBox.setBounds(map.getBounds());
-        });
+        // map.addListener('bounds_changed', function () {
+        //     searchBox.setBounds(map.getBounds());
+        // });
 
         // Layer with all the hotel location markers
         //    let ctaLayer = new google.maps.KmlLayer({
@@ -109,34 +188,53 @@ export class GoogleMapComponent implements OnInit {
                 return;
             }
 
+            console.log(places.length);
+
+
             // Clear out the old markers.
-            markers.forEach(function (marker) {
-                marker.setMap(null);
-            });
-            markers = [];
+            // markers.forEach(function (marker) {
+            //     marker.setMap(null);
+            // });
+            // markers = [];
+            // if (that.centerMarker !== undefined) {
+            //     that.centerMarker.setMap(null);
+            // }
+            
 
             // For each place, get the icon, name and location.
             var bounds = new google.maps.LatLngBounds();
             places.forEach(function (place) {
+                console.log(place);
+
                 if (!place.geometry) {
                     console.log("Returned place contains no geometry");
                     return;
                 }
-                var icon = {
-                    url: place.icon,
-                    size: new google.maps.Size(71, 71),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 34),
-                    scaledSize: new google.maps.Size(25, 25)
-                };
-
+                // var icon = {
+                //     url: place.icon,
+                //     size: new google.maps.Size(71, 71),
+                //     origin: new google.maps.Point(0, 0),
+                //     anchor: new google.maps.Point(17, 34),
+                //     scaledSize: new google.maps.Size(25, 25)
+                // };
+                console.log(place.geometry.location);
+                
+                that.addCenter(map, place.geometry.location);
                 // Create a marker for each place.
-                markers.push(new google.maps.Marker({
-                    map: map,
-                    icon: icon,
-                    title: place.name,
-                    position: place.geometry.location
-                }));
+
+                // that.centerMarker = new google.maps.Marker({
+                //     map: map,
+                //     icon: icon,
+                //     title: place.name,
+                //     position: place.geometry.location
+                // });
+
+                // markers.push(new google.maps.Marker({
+                //     map: map,
+                //     icon: icon,
+                //     title: place.name,
+                //     position: place.geometry.location
+                // }));
 
                 if (place.geometry.viewport) {
                     // Only geocodes have viewport.
@@ -152,6 +250,7 @@ export class GoogleMapComponent implements OnInit {
     relocate() {
         console.log('relocateing...');
         var map = this.myMap;
+        var that = this;
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function (position) {
                 console.log(position);
@@ -159,6 +258,7 @@ export class GoogleMapComponent implements OnInit {
                     center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
                     zoom: 12
                 });
+                that.addCenter(map, new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
                 // map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
             }, function () {
                 console.log('can not get position, default is nyc ');
@@ -166,6 +266,7 @@ export class GoogleMapComponent implements OnInit {
                     center: new google.maps.LatLng(40.6945088, -73.9871052),
                     zoom: 12
                 });
+                that.addCenter(map, new google.maps.LatLng(40.6945088, -73.9871052));
                 // map.setCenter(new google.maps.LatLng(40.6945088, -73.9871052));
             }, { enableHighAccuracy: false, maximumAge: Infinity, timeout: 5000 });
         }
